86f458be53c4c075ce099f9dab8d4089341a17b4
James Kylstra <kylstraj@oregonstate.edu>
2019-05-26 09:33:23 -0600

    deploy router build

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.3d700a7180f88b02e39ac98093bacf8a.js"
 );
 
 self.addEventListener('message', (event) => {
